607d379a0b7bec451e6d442f9bb27c99a8b1218a
Whit Jackson <whit@whitjack.me>
2015-10-21 15:06:29 -0700

    Updates

--- js/menu.js
+++ js/menu.js
@@ -55,7 +55,7 @@ var Menu = (function() {
 				description = recipe.parent().next().text(),
 				url = recipe.attr( 'href' );
 
-			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><a href="' + url + '">See the recipe</a><span class="rm-close-modal">x</span></div>' );
+			var $modal = $( '<div class="rm-modal"><div class="rm-thumb" style="background-image: url(' + img + ')"></div><h5>' + title + '</h5><p>' + description + '</p><span class="rm-close-modal">x</span></div>' );
 
 			$modal.appendTo( $container );
 
